60d746411d61475a1f66c6855d0156064ac7db49
Luke Sutor <64111222+LukeSutor@users.noreply.github.com>
2022-01-15 19:54:58 -0500

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -1,6 +1,7 @@
 from torch import nn
 import torch.nn.functional as F
 
+# This cnn model isn't being used, as this model was trained using Timm's efficientnet2 small model
 
 class CNNNetwork(nn.Module):
 
@@ -56,22 +57,6 @@ class CNNNetwork(nn.Module):
         self.linear = nn.Linear(17920, 4)  # Output is set to 3 for now,
         # this value is the number of guitarists, edit later
         self.softmax = nn.Softmax(dim=1)
-        # super(CNNNetwork, self).__init__()
-        # self.conv1 = nn.Conv2d(1, 128, 3, 1, 2)
-        # self.bn1 = nn.BatchNorm2d(128)
-        # self.pool1 = nn.MaxPool2d(4)
-        # self.conv2 = nn.Conv2d(128, 128, 3)
-        # self.bn2 = nn.BatchNorm2d(128)
-        # self.pool2 = nn.MaxPool2d(4)
-        # self.conv3 = nn.Conv2d(128, 256, 3)
-        # self.bn3 = nn.BatchNorm2d(256)
-        # self.pool3 = nn.MaxPool2d(4)
-        # self.conv4 = nn.Conv2d(256, 512, 3)
-        # self.bn4 = nn.BatchNorm2d(512)
-        # self.pool4 = nn.MaxPool2d(4)
-        # # input should be 512x30 so this outputs a 512x1
-        # self.avgPool = nn.AvgPool1d(30)
-        # self.fc1 = nn.Linear(512, 4)
 
     def forward(self, input_data):
         x = self.conv1(input_data)
@@ -82,22 +67,6 @@ class CNNNetwork(nn.Module):
         logits = self.linear(x)
         predictions = self.softmax(logits)
         return predictions
-        x = self.conv1(input_data)
-        x = F.relu(self.bn1(x))
-        x = self.pool1(x)
-        x = self.conv2(x)
-        x = F.relu(self.bn2(x))
-        x = self.pool2(x)
-        x = self.conv3(x)
-        x = F.relu(self.bn3(x))
-        x = self.pool3(x)
-        x = self.conv4(x)
-        x = F.relu(self.bn4(x))
-        x = self.pool4(x)
-        x = self.avgPool(x)
-        x = x.permute(0, 2, 1)  # change the 512x1 to 1x512
-        x = self.fc1(x)
-        return F.log_softmax(x, dim=2)
 
 
 if __name__ == "__main__":
